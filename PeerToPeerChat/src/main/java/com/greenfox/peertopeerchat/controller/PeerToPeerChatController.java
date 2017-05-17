f51e06e0baa211d0a7a0209533879e998f51d9fa
Zsuzsanna Padar <zsuzsanna.padar@gmail.com>
2017-05-17 11:08:25 +0200

    init(PeerToPeerController):index.html

--- PeerToPeerChat/src/main/java/com/greenfox/peertopeerchat/controller/PeerToPeerChatController.java
+++ PeerToPeerChat/src/main/java/com/greenfox/peertopeerchat/controller/PeerToPeerChatController.java
@@ -1,8 +1,21 @@
 package com.greenfox.peertopeerchat.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 /**
  * Created by zsuzsanna.padar on 2017. 05. 17..
  */
+@Controller
 public class PeerToPeerChatController {
+
+  @GetMapping(value = "/")
+  public String getHomePage(){
+    return "index";
+  }
+
+
 
 }
